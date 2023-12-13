bc078ca28b23166369dada481994e2a227a4eee5
deremenko <eremenko.fortech@gmail.com>
2023-12-14 02:51:02 +0300

    Ex_15 solution

---
+++ Ex_15.js
@@ -0,0 +1,7 @@
+const generateXLine = (xLineNumber)=>{
+    let returnArray = [];
+    for (let iteration = 1; iteration<= xLineNumber;iteration++) {
+        returnArray.push('x'.repeat(iteration));
+    }
+    return returnArray
+}
