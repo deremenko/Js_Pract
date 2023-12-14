785a4e54594e8a53453604ff20fa0a41f002fd5b
deremenko <eremenko.fortech@gmail.com>
2023-12-14 18:17:49 +0300

    fix tab

--- Ex_1.js
+++ Ex_1.js
@@ -1,17 +1,15 @@
-// Решение через использование метода .sort
+
 const sortElements = (arr, direction) => {
   switch (direction) {
-      case '<': 
-          arr.sort((a,b)=>{
-              return a-b
-          })
-          break
-      case '>':
-         arr.sort((a,b)=>{
-              return b-a
-          })
-          break
-      default:
-          console.log("error direction")
-}
+    case '<': 
+      arr.sort((a,b)=>{
+        return a-b;
+      })
+      break;
+    case '>':
+      arr.sort((a,b)=>{
+        return b-a;
+      })
+      break;
+  }
 }
