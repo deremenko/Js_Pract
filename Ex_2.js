1c1e33bfe974716dcec91dad612eaf31b92beaa7
deremenko <eremenko.fortech@gmail.com>
2023-12-14 18:28:57 +0300

    fix tab and name function

--- Ex_2.js
+++ Ex_2.js
@@ -1,21 +1,7 @@
-// Вариант через цикл
-const cutStr = (str)=>{
-   if (str.length>5) {
-       return str.slice(0,3)+str.slice(-3)
-   }else {
-       let charStr = '';
-       for (let i=0; i<str.length; i++) {
-        charStr += str[0];
-       }
-       return charStr
-    }
-}
-
-// Варианет через split
-const cutStrs = (str)=>{
-    if (str.length>5) {
-        return str.slice(0,3)+str.slice(-3)
-    }else {
-        return str.split('').forEach((iten)=>{item=str[0]}).join('')
-     }
- }
+const trimmerLine = (str) => {
+  if (str.length>5) {
+    return str.slice(0,3)+str.slice(-3);
+  }else {
+    return str[0].repeat(str.length);
+  }
+ }  
