3746613241e3fcee81433aa06901df2f92749e23
deremenko <eremenko.fortech@gmail.com>
2023-12-16 19:07:11 +0300

    Code style correction and function improvement

--- Ex_8.js
+++ Ex_8.js
@@ -1,12 +1,8 @@
-const sortFiledArr = (arr,field,mod='asc')=>{
-    switch (mod){
-        case 'asc':
-            return arr.sort((a,b)=>a[field]-b[field]);
-            break;
-        case 'desc':
-            return arr.sort((a,b)=>b[field]-a[field]);
-            break;
-        default:
-            console.log("This mode is not available in this function")
-    }
-}
+const sortFieldArray = (arr, field, direction = 'asc') => {
+  switch (direction) {
+    case 'asc':
+      return arr.sort((a, b) => a[field] - b[field]);
+    case 'desc':
+      return arr.sort((a, b) => b[field] - a[field]);
+  }
+};
