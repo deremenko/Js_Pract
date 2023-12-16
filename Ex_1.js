d0d473813896efc3a22e48415e4443e6cc9e2710
deremenko <eremenko.fortech@gmail.com>
2023-12-16 17:30:56 +0300

    code style fixes

--- Ex_1.js
+++ Ex_1.js
@@ -2,12 +2,12 @@
 const sortElements = (arr, direction) => {
   switch (direction) {
     case 'asc': 
-      arr.sort((a,b)=>{
+      arr.sort((a, b) => {
         return a - b;
       })
       break;
     case 'desc':
-      arr.sort((a,b)=>{
+      arr.sort((a, b) => {
         return b - a;
       })
       break;
