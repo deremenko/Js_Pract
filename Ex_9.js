2e9492e80cee32ab6118fd9428660340f4b10753
deremenko <eremenko.fortech@gmail.com>
2023-12-16 19:17:42 +0300

    Code style correction and function improvement

--- Ex_9.js
+++ Ex_9.js
@@ -1,8 +1,15 @@
-const findingCommonValues = (arrOne,arrTwo)=>{
-    let arr = []
-    if (arrOne.length>arrTwo.length) [arrOne,arrTwo] = [arrTwo,arrOne];
-    arrOne.forEach((item)=>{
-        if (arrTwo.includes(item)) arr.push(item)
-    })
-    return arr
-}
+const findCommonValues = (arrOne, arrTwo) => {
+  const arr = [];
+
+  if (arrOne.length > arrTwo.length) {
+    [arrOne, arrTwo] = [arrTwo, arrOne];
+  }
+
+  arrOne.forEach((item) => {
+    if (arrTwo.includes(item)) {
+      arr.push(item);
+    }
+  });
+
+  return arr;
+};
