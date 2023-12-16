5f28e78075c2ace400f049dc292c3a38cd405d9b
deremenko <eremenko.fortech@gmail.com>
2023-12-16 17:34:33 +0300

    Style fix

--- Ex_2.js
+++ Ex_2.js
@@ -1,7 +1,8 @@
 const trimmerLine = (str) => {
-  if (str.length>5) {
-    return str.slice(0,3)+str.slice(-3);
-  }else {
+  if (str.length > 5) {
+    return str.slice(0, 3) + str.slice(-3);
+  } else {
     return str[0].repeat(str.length);
   }
- }  
+}
+  
