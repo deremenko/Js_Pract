4f028026c9a58f7566db9fb430674cb6bc82b17f
deremenko <eremenko.fortech@gmail.com>
2023-12-16 19:47:34 +0300

    Code style correction and function improvement

--- Ex_12.js
+++ Ex_12.js
@@ -1,7 +1,6 @@
-const indexArr = (arr)=>{
-    let minIndex = arr.indexOf(Math.min(...arr));
-    let maxIndex = arr.indexOf(Math.max(...arr));
-    [arr[minIndex], arr[maxIndex]] = [arr[maxIndex], arr[minIndex]];
-    return arr
-    
-}
+const swapMinMaxElements = (arr) => {
+  const minIndex = arr.indexOf(Math.min(...arr));
+  const maxIndex = arr.indexOf(Math.max(...arr));
+  [arr[minIndex], arr[maxIndex]] = [arr[maxIndex], arr[minIndex]];
+  return arr;
+};
