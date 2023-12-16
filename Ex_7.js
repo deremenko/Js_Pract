80ab1b42041362d2c81816a97b3e97a99a7aa35b
deremenko <eremenko.fortech@gmail.com>
2023-12-16 18:59:28 +0300

    Code style correction and function improvement

--- Ex_7.js
+++ Ex_7.js
@@ -1,17 +1,19 @@
 
-const sortArr = (arrRaw)=>{
-    let bufferArr = [];
-    let bufferNumber = 0;
-    
-    arrRaw.forEach((item,index)=>{
-        for (let key in item){
-            if (item[key] !== '' && item[key] !== null) {
-                bufferNumber += 1;
-            }
-        }
-        bufferArr.push([bufferNumber,index])
-        bufferNumber = 0;
-    })
-    bufferArr.sort((a,b)=>b[0]-a[0])
-    return arrRaw = bufferArr.map(index => arrRaw[index[1]]);
-}
+const sortArr = (arrRaw) => {
+  let bufferArr = [];
+  let bufferNumber = 0;
+
+  arrRaw.forEach((item, index) => {
+    for (let key in item) {
+      if (item[key] !== '' && item[key] !== null) {
+        bufferNumber += 1;
+      }
+    }
+    if (bufferNumber > 0) bufferArr.push([bufferNumber, index]);
+    bufferNumber = 0;
+  });
+  bufferArr.sort((a, b) => b[0] - a[0]);
+
+  return arrRaw = bufferArr.map((index) => arrRaw[index[1]]);
+};
+
