cb8b6a09b25cdc528741d4515bd5aedb951a6d54
Denis Eremenko <eremenko.fortech@gmail.com>
2023-12-20 12:47:52 +0300

    laste fix

--- Ex_8.js
+++ Ex_8.js
@@ -1,8 +1,13 @@
 const sortFieldArray = (arr, field, direction = 'asc') => {
   switch (direction) {
     case 'asc':
-      return arr.sort((a, b) => a[field] - b[field]);
+      return typeof arr[0][field] === 'string'
+        ? arr.sort((a, b) => a[field].localeCompare(b[field]))
+        : arr.sort((a, b) => a[field] - b[field]);
     case 'desc':
-      return arr.sort((a, b) => b[field] - a[field]);
+      return typeof arr[0][field] === 'string'
+        ? arr.sort((a, b) => b[field].localeCompare(a[field]))
+        : arr.sort((a, b) => b[field] - a[field]);
   }
 };
+
