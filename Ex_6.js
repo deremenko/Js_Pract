e4a12ec2688f3a2a232d12553d8b0a6c464d8726
deremenko <eremenko.fortech@gmail.com>
2023-12-22 06:00:41 +0300

    last fix for merge

--- Ex_6.js
+++ Ex_6.js
@@ -3,12 +3,11 @@ const sortingProperties = (objRaw) => {
   
   objRaw.forEach((item) => {
     const existingItem = bufferArr.find((element) => element['name'] === item['name']);
-    
     existingItem
-    ? existingItem['price'] += item['price'] 
-    : bufferArr.push(item);
+      ? existingItem['price'] += item['price'] 
+      : bufferArr.push(item);
     });
   
   return bufferArr;
-  };
+};
   
